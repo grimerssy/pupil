0c39c729a94bed885fbe178a1aedea300cfb0b5f
Stanislav Stoyanov <grimerssy@gmail.com>
2025-04-24 11:43:57 +0200

    tuple syntax in internal error's debug impl

--- src/error.rs
+++ src/error.rs
@@ -17,8 +17,19 @@ pub struct InternalError(#[from] anyhow::Error);
 
 impl fmt::Debug for InternalError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{self}")?;
-        core::iter::successors(self.0.source(), |src| src.source())
+        f.debug_tuple("InternalError")
+            .field(&ErrorChain(&self.0))
+            .finish()
+    }
+}
+
+struct ErrorChain<'a>(&'a anyhow::Error);
+
+impl fmt::Debug for ErrorChain<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let error = self.0;
+        write!(f, "{error}")?;
+        core::iter::successors(error.source(), |src| src.source())
             .try_for_each(|src| write!(f, ": {src}"))
     }
 }
