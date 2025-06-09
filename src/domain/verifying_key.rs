693f582365eb6983c5a1299cb3caff6c5333b358
Stanislav Stoyanov <grimerssy@gmail.com>
2025-06-09 15:47:13 +0200

    serialize verifying key as hex

--- src/domain/verifying_key.rs
+++ src/domain/verifying_key.rs
@@ -1,10 +1,25 @@
 use serde::Serialize;
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone)]
 pub struct VerifyingKey(ed25519_dalek::VerifyingKey);
 
 impl VerifyingKey {
     pub fn new(key: ed25519_dalek::VerifyingKey) -> Self {
         Self(key)
+    }
+}
+
+impl Serialize for VerifyingKey {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        #[allow(clippy::format_collect)]
+        self.0
+            .to_bytes()
+            .iter()
+            .map(|byte| format!("{byte:02X}"))
+            .collect::<String>()
+            .serialize(serializer)
     }
 }
